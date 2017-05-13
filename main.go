b2b2d6f808fb4f6fe4a440aa7e85971e2dd9b45c
Zsolt <szokezsolt88@gmail.com>
2017-05-13 14:57:34 +0200

    Update main.go

--- main.go
+++ main.go
@@ -4,8 +4,9 @@ import (
 	"errors"
 	"fmt"
 	"os"
-
 	"github.com/bitrise-io/go-utils/log"
+	"github.com/bitrise-io/go-utils/fileutil"
+	"github.com/bitrise-io/go-utils/pathutil"
 )
 
 // ConfigsModel ...
@@ -17,6 +18,35 @@ func createConfigsModelFromEnvs() ConfigsModel {
 	return ConfigsModel{
 		ExampleInput: os.Getenv("example_step_input"),
 	}
+}
+
+func downloadFile(downloadURL, targetPath string) error {
+	outFile, err := os.Create(targetPath)
+	if err != nil {
+		return fmt.Errorf("failed to create (%s), error: %s", targetPath, err)
+	}
+	defer func() {
+		if err := outFile.Close(); err != nil {
+			log.Warnf("Failed to close (%s)", targetPath)
+		}
+	}()
+
+	resp, err := http.Get(downloadURL)
+	if err != nil {
+		return fmt.Errorf("failed to download from (%s), error: %s", downloadURL, err)
+	}
+	defer func() {
+		if err := resp.Body.Close(); err != nil {
+			log.Warnf("failed to close (%s) body", downloadURL)
+		}
+	}()
+
+	_, err = io.Copy(outFile, resp.Body)
+	if err != nil {
+		return fmt.Errorf("failed to download from (%s), error: %s", downloadURL, err)
+	}
+
+	return nil
 }
 
 func (configs ConfigsModel) print() {
@@ -46,7 +76,11 @@ func main() {
 
 	fmt.Println()
 
-	//
 	// Main
-	log.Printf("Hello world!")
+	
+	// downloadUrl := DOWNLOAD URL TO BE IMPORTED SOMEHOW FROM STEP.YML
+	// downloadPth := TEMPORARY DIRECTORY PATH TO BE DOWNLOADED TO
+	if err := downloadFile(downloadUrl, downloadPth); err != nil {
+		failf("Failed to download json file, error: %s", err)
+	}
 }
